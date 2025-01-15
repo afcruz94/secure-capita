49063d36b8cf4a1bf82610275da518e4119cd3cf
afcruz94 <132563046+afcruz94@users.noreply.github.com>
2025-01-15 00:02:13 +0000

    add delete user roles query and queries order

--- src/main/java/com/afcruz/securecapita/query/RoleQuery.java
+++ src/main/java/com/afcruz/securecapita/query/RoleQuery.java
@@ -1,6 +1,7 @@
 package com.afcruz.securecapita.query;
 
 public class RoleQuery {
-    public final static String INSERT_ROLE_TO_USER = "INSERT INTO UserRoles (user_id, role_id) VALUES (:userId, :roleId)";
     public final static String SELECT_ROLE_BY_NAME_QUERY = "SELECT * FROM Roles WHERE name = :name";
+    public final static String INSERT_ROLE_TO_USER_QUERY = "INSERT INTO UserRoles (user_id, role_id) VALUES (:userId, :roleId)";
+    public final static String DELETE_ROLE_TO_USER_QUERY = "DELETE FROM UserRoles WHERE user_id = :userId";
 }
