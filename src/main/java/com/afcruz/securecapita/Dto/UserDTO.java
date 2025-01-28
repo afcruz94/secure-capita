aaf08d75b44b75360f61049196a82c4224d0446e
afcruz94 <132563046+afcruz94@users.noreply.github.com>
2025-01-28 21:23:06 +0000

    add annotations to UserDTO

--- src/main/java/com/afcruz/securecapita/Dto/UserDTO.java
+++ src/main/java/com/afcruz/securecapita/Dto/UserDTO.java
@@ -1,16 +1,25 @@
 package com.afcruz.securecapita.Dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
 public class UserDTO {
     private Long userId;
+    @NotEmpty(message = "Field cannot be empty")
     private String firstName;
+    @NotEmpty(message = "Field cannot be empty")
     private String lastName;
+    @NotEmpty(message = "Field cannot be empty")
+    @Email(message = "Invalid Email. Please enter a valid email address")
     private String email;
+    private String password;
     private String address;
     private String phone;
     private String title;
